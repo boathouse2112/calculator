d9e9d9659744d70d91d657ed5c3b2035c22de30d
Mark Murphy <murphy.mark@northeastern.edu>
2022-03-11 20:22:37 -0500

    Done

--- calculator.js
+++ calculator.js
@@ -1,4 +1,9 @@
 
+const DISPLAY_WIDTH = 8;
+let displayValue = 0;
+
+const calculatorDisplay = document.getElementById("display");
+
 const add = function(a, b) {
     return a + b;
 }
@@ -27,3 +32,27 @@ const operate = function(op, a, b) {
             return divide(a, b);
     }
 }
+
+const addDigit = function(n, newDigit) {
+    // Don't add to an 8-digit number
+    if (displayValue.toString().length >= 8) {
+        return;
+    } else {
+        return n * 10 + newDigit;
+    }
+}
+
+// Updates displayValue and pushes it to the document
+const updateDisplayValue = function(newValue) {
+    displayValue = newValue;
+    calculatorDisplay.innerText = displayValue;
+}
+
+const updateDisplay = function(buttonID) {
+    switch (buttonID) {
+        case "oneButton":
+            updateDisplayValue(addDigit(displayValue, 1));
+    }
+}
+
+
